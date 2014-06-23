ed0305ea9c886e70a3e8ff72b792a3c4a7c13bb5
Makis Otman <makisotman@gmail.com>
2014-06-23 09:10:04 +0100

    Refactored array specs in the same manner

--- array/spec/kitchen_spec.rb
+++ array/spec/kitchen_spec.rb
@@ -1,50 +1,50 @@
 require 'spec_helper'
 
 describe Kitchen do
-  let (:kitchen) {Kitchen.new}
+  let (:kitchen) { Kitchen.new }
 
   it "initializes with an empty items array" do
-    kitchen.items.should == []
+    expect(kitchen.items).to eq []
   end
 
-  it "uses the shovel operator to add a stove to the kitchen" do
-    kitchen.add("stove") == ["stove"]
+  xit "uses the shovel operator to add a stove to the kitchen" do
+    expect(kitchen.add("stove")).to eq ["stove"]
   end
 
-  it "returns the first and last emements of the array" do
+  xit "returns the first and last emements of the array" do
     kitchen.add("stove")
     kitchen.add("cat")
     kitchen.add("sink")
     kitchen.remove_first_and_last_elements
 
-    kitchen.items.should == ["cat"]
+    expect(kitchen.items).to eq ["cat"]
   end
 
-  it "adds and ! to the end of all of its items" do
+  xit "adds and ! to the end of all of its items" do
     kitchen.add("stove")
     kitchen.add("windows")
     kitchen.add_bang
 
-    kitchen.items.should == ["stove!", "windows!"]
+    expect(kitchen.items).to eq ["stove!", "windows!"]
   end
 
-  it "removes items that don't belong in the kitchen" do
-    kitchen.remove_with_slice(["stove", "rat", "shoe", "pan"]).should == ["stove, pan"]
+  xit "removes items that don't belong in the kitchen" do
+    expect(kitchen.remove_with_slice(["stove", "rat", "shoe", "pan"])).to eq ["stove, pan"]
   end
 
-  it "creates arrays out of each element" do
-    kitchen.make_mini_arrays(["stove", "windows"]).should == [["stove"], ["windows"]]
+  xit "creates arrays out of each element" do
+    expect(kitchen.make_mini_arrays(["stove", "windows"])).to eq [["stove"], ["windows"]]
   end
 
-  it "removes any item with more than 3 letters in its name from the kitchen" do
-    kitchen.remove_if_more_than_three_letters(["windows", "cat", "stove", "pan", "scarf"]).should == ["cat", "pan"]
+  xit "removes any item with more than 3 letters in its name from the kitchen" do
+    expect(kitchen.remove_if_more_than_three_letters(["windows", "cat", "stove", "pan", "scarf"])).to eq ["cat", "pan"]
   end
 
-  it "sorts each item alphabetically backwards " do
-    kitchen.alphabetically_backwards(["apple", "cat", "window", "pan"]).should == ["window", "pan","cat", "apple"]
+  xit "sorts each item alphabetically backwards " do
+    expect(kitchen.alphabetically_backwards(["apple", "cat", "window", "pan"])).to eq ["window", "pan","cat", "apple"]
   end
 
-  it "returns the first 3 items in the kitchen" do
-    kitchen.first_three(["cake", "cat", "kale", "pan"]).should == ["cake", "cat", "kale"]
+  xit "returns the first 3 items in the kitchen" do
+    expect(kitchen.first_three(["cake", "cat", "kale", "pan"])).to eq ["cake", "cat", "kale"]
   end
 end
