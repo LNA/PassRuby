6209298a758b0d177349f847ad14afe7a09a9122
LNA <LAllen@8thLight.com>
2014-09-19 11:02:06 -0500

    Adds code smells to movie finder

--- lib/refactor/movie_finder.rb
+++ lib/refactor/movie_finder.rb
@@ -1,6 +1,6 @@
-class MovieFinder # What happens if I want to add in another day of the week? Is this good or bad?
+class MovieFinder # What happens if I want to add in another day of the week? Is this good or bad? 
   def find_movie (day_of_week)
-    if day_of_week == "Sunday"
+    if day_of_week    == "Sunday"
       "Comedy"
     elsif day_of_week == "Monday"
       "Family"
@@ -13,4 +13,20 @@ class MovieFinder # What happens if I want to add in another day of the week? Is
 
   def find_movie_refactored(day_of_week)
   end
+
+  def get_title_and_cost(genera)
+    if genera == "Kids"
+      cost = 3.00
+      movie = "The Princess Bride"
+    end
+    [movie, cost]
+  end
+
+  # the below methods are private because they will only be used in this class.  The above methods are public.
+  # Private methods are not tested. 
+
+  private
+
+  # Fill in some private methods that will remove the hard coded values from the find_movie method.
+  # They don't need to be tested.
 end
--- spec/refactor/movie_finder_spec.rb
+++ spec/refactor/movie_finder_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 # Green:    Write the least amount of code to make the test pass; thats done too.
 
-# Refactor: We are going to be removing conditionals, duplication, and hard coded values.
+# Refactor: We are going to be removing code smells..
 
 describe MovieFinder do
   let (:finder) { MovieFinder.new }
@@ -13,7 +13,23 @@ describe MovieFinder do
     expect(finder.find_movie("Wednesday")).to eq ("Drama")
   end
 
-  it "refactors code to do get rid of all of those pesky conditionals" do 
+  it "refactors code to do get rid of all of those pesky conditionals and hard coded values." do 
     expect(finder.find_movie_refactored("Wednesday")).to eq ("Drama")
+  end
+
+  it "gets the name of the movie and calculates the streaming download cost" do 
+    expect(finder.get_title_and_cost("Kids")).to eq(["The Princess Bride", 3.00])
+  end
+
+  it "refactors out a method to get a name" do 
+    expect(finder.get_title("Kids")).to eq("The Princess Bride")
+  end
+
+  it "refactors out a method to get the cost of a movie" do 
+    expect(finder.get_cost("Kids")).to eq(3.00)
+  end
+
+  it "returns a hash of the movie and its cost when given a genera" do 
+    expect(finder.my_streaming_history["Kids"]).to eq({"The Princess Bride" => 3.00})
   end
 end 
